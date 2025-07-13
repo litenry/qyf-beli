41eb21ccdee99f91c4b5859ce0602d357e4cc1d2
litenry <beisgofe@gmail.com>
2025-07-13 15:45:40 -0400

    script.js

--- script.js
+++ script.js
@@ -1,100 +1,115 @@
-const ramos = [
-  // Semestre 1
-  { nombre: "Química General I", id: "qg1", prerequisitos: [] },
-  { nombre: "Matemáticas", id: "mate", prerequisitos: [] },
-  { nombre: "Biología Celular", id: "bc", prerequisitos: [] },
-  { nombre: "Introducción al Laboratorio", id: "lab", prerequisitos: [] },
-  { nombre: "Introducción a la Química y Farmacia", id: "iqf", prerequisitos: [] },
-  { nombre: "Anatomía", id: "anat", prerequisitos: [] },
-  { nombre: "Taller Nivelación Lengua Materna", id: "leng", prerequisitos: [] },
-
-  // Semestre 2
-  { nombre: "Química General II", id: "qg2", prerequisitos: ["qg1", "lab"] },
-  { nombre: "Cálculo", id: "cal", prerequisitos: ["mate"] },
-  { nombre: "Fisiología I", id: "fisio1", prerequisitos: ["bc", "anat"] },
-  { nombre: "Química Orgánica I", id: "qo1", prerequisitos: ["qg1", "lab"] },
-  { nombre: "Introducción a la Estadística", id: "est", prerequisitos: ["mate"] },
-  { nombre: "Taller Nivelación Inglés", id: "ing", prerequisitos: [] },
-
-  // Semestre 3
-  { nombre: "Química Orgánica II", id: "qo2", prerequisitos: ["qg2", "qo1"] },
-  { nombre: "Química Analítica", id: "qa", prerequisitos: ["qg2", "cal"] },
-  { nombre: "Fisiología II", id: "fisio2", prerequisitos: ["fisio1"] },
-  { nombre: "Administración de Organizaciones", id: "admin", prerequisitos: ["mate", "leng"] },
-  { nombre: "Bioquímica", id: "bioq", prerequisitos: ["bc", "qo1"] },
-  { nombre: "Ética", id: "etica", prerequisitos: ["leng"] },
-
-  // Semestre 4
-  { nombre: "Botánica", id: "bota", prerequisitos: ["bc", "qo1"] },
-  { nombre: "Análisis Instrumental", id: "ai", prerequisitos: ["mate"] },
-  { nombre: "Fisiopatología", id: "fisiop", prerequisitos: ["fisio2"] },
-  { nombre: "Fisicoquímica", id: "fq", prerequisitos: ["qg2", "cal"] },
-  { nombre: "Administración de Recursos Humanos", id: "rrhh", prerequisitos: ["admin", "etica"] },
-  { nombre: "Introducción a la Química Farmacéutica", id: "iqf2", prerequisitos: ["qo2"] },
-
-  // Semestre 5
-  { nombre: "Farmacognosia", id: "fco", prerequisitos: ["qo2", "bota"] },
-  { nombre: "Biología Molecular", id: "bm", prerequisitos: ["bioq"] },
-  { nombre: "Operaciones Unitarias", id: "ou", prerequisitos: ["fq"] },
-  { nombre: "Asignatura Electiva Profesional", id: "electiva1", prerequisitos: ["etica"] },
-  { nombre: "Microbiología", id: "micro", prerequisitos: ["bioq"] },
-  { nombre: "Legislación Farmacéutica", id: "leg", prerequisitos: ["admin"] },
-
-  // Semestre 6
-  { nombre: "Biofarmacia", id: "biof", prerequisitos: ["ai", "fisio2"] },
-  { nombre: "Farmacología I", id: "farma1", prerequisitos: ["fisiop", "iqf2"] },
-  { nombre: "Inmunología", id: "inmu", prerequisitos: ["bm", "fisiop"] },
-  { nombre: "Química Farmacéutica II", id: "qf2", prerequisitos: ["iqf2", "micro"] },
-  { nombre: "Práctica Profesional Farmacia I", id: "ppf1", prerequisitos: ["rrhh", "etica", "leg"] },
-  { nombre: "Bioestadística", id: "bioest", prerequisitos: ["mate", "etica"] },
-
-  // Semestre 7
-  { nombre: "Tecnología Farmacéutica I", id: "tf1", prerequisitos: ["biof", "ou"] },
-  { nombre: "Farmacología II", id: "farma2", prerequisitos: ["farma1", "qf2"] },
-  { nombre: "Química Fisiológica y Patológica", id: "qfp", prerequisitos: ["ai", "inmu"] },
-  { nombre: "Salud Pública y Epidemiología", id: "spe", prerequisitos: ["etica", "bioest"] },
-  { nombre: "Química de Alimentos", id: "qa2", prerequisitos: ["ai"] },
-  { nombre: "Metodología de la Investigación", id: "met", prerequisitos: ["etica", "bioest"] },
-  { nombre: "Taller Integración Perfil de Egreso I", id: "tipe1", prerequisitos: ["ppf1"] },
-
-  // Semestre 8
-  { nombre: "Tecnología Farmacéutica II", id: "tf2", prerequisitos: ["tf1"] },
-  { nombre: "Farmacia Asistencial", id: "farmas", prerequisitos: ["spe"] },
-  { nombre: "Nutrición", id: "nut", prerequisitos: ["qfp", "qa2"] },
-  { nombre: "Gestión de Calidad", id: "gcal", prerequisitos: ["tf1", "leg"] },
-  { nombre: "Bioquímica Clínica", id: "bioqc", prerequisitos: ["qfp"] },
-  { nombre: "Seminario de Licenciatura", id: "sem", prerequisitos: ["tf1", "farma2", "qfp", "spe", "qa2", "met", "tipe1"] },
-  { nombre: "Taller Integración Perfil de Egreso II", id: "tipe2", prerequisitos: ["tipe1"] },
-
-  // Semestre 9
-  { nombre: "Farmacia Clínica I", id: "fc1", prerequisitos: ["tf2", "farmas", "nut", "gcal", "bioqc", "sem", "tipe2"] },
-  { nombre: "Cosmética", id: "cos", prerequisitos: ["tf2"] },
-  { nombre: "Toxicología", id: "tox", prerequisitos: ["farma2"] },
-  { nombre: "Gestión Comercial", id: "gcom", prerequisitos: ["gcal", "rrhh"] },
-  { nombre: "Medicamentos Complementarios", id: "medcomp", prerequisitos: ["farma2", "nut"] },
-  { nombre: "Práctica Profesional II", id: "pp2", prerequisitos: ["farma2", "ppf1"] },
-  { nombre: "Taller Integración Perfil de Egreso III", id: "tipe3", prerequisitos: ["tipe2"] },
-
-  // Semestre 10
-  { nombre: "Asignatura Electiva Profesional", id: "electiva2", prerequisitos: ["etica"] },
-  { nombre: "Farmacia Clínica", id: "fc2", prerequisitos: ["fc1"] },
-  { nombre: "Unidad de Investigación o Internado I", id: "ui1", prerequisitos: ["met", "sem"] },
-  { nombre: "Práctica Profesional III", id: "pp3", prerequisitos: ["tf2", "ai", "farmas", "tox", "qa2", "bioqc"] },
-
-  // Semestre 11
-  { nombre: "Unidad de Investigación o Internado II", id: "ui2", prerequisitos: ["ui1"] }
-];
+const ramosPorSemestre = {
+  "Semestre 1": [
+    { nombre: "Química General I", prerequisitos: [] },
+    { nombre: "Matemáticas", prerequisitos: [] },
+    { nombre: "Biología Celular", prerequisitos: [] },
+    { nombre: "Introducción al Laboratorio", prerequisitos: [] },
+    { nombre: "Introducción a la Química y Farmacia", prerequisitos: [] },
+    { nombre: "Anatomía", prerequisitos: [] },
+    { nombre: "Taller Nivelación Lengua Materna", prerequisitos: [] }
+  ],
+  "Semestre 2": [
+    { nombre: "Química General II", prerequisitos: ["Química General I", "Introducción al Laboratorio"] },
+    { nombre: "Cálculo", prerequisitos: ["Matemáticas"] },
+    { nombre: "Fisiología I", prerequisitos: ["Biología Celular", "Anatomía"] },
+    { nombre: "Química Orgánica I", prerequisitos: ["Química General I", "Introducción al Laboratorio"] },
+    { nombre: "Introducción a la Estadística", prerequisitos: ["Matemáticas"] },
+    { nombre: "Taller Nivelación Inglés", prerequisitos: [] }
+  ],
+  "Semestre 3": [
+    { nombre: "Química Orgánica II", prerequisitos: ["Química General II", "Química Orgánica I"] },
+    { nombre: "Química Analítica", prerequisitos: ["Química General II", "Cálculo"] },
+    { nombre: "Fisiología II", prerequisitos: ["Fisiología I"] },
+    { nombre: "Administración de Organizaciones", prerequisitos: ["Matemáticas", "Taller Nivelación Lengua Materna"] },
+    { nombre: "Bioquímica", prerequisitos: ["Biología Celular", "Química Orgánica I"] },
+    { nombre: "Ética", prerequisitos: ["Taller Nivelación Lengua Materna"] }
+  ],
+  "Semestre 4": [
+    { nombre: "Botánica", prerequisitos: ["Biología Celular", "Química Orgánica I"] },
+    { nombre: "Análisis Instrumental", prerequisitos: ["Matemáticas"] },
+    { nombre: "Fisiopatología", prerequisitos: ["Fisiología II"] },
+    { nombre: "Fisicoquímica", prerequisitos: ["Química General II", "Cálculo"] },
+    { nombre: "Administración de Recursos Humanos", prerequisitos: ["Administración de Organizaciones", "Ética"] },
+    { nombre: "Introducción a la Química Farmacéutica", prerequisitos: ["Química Orgánica II"] }
+  ],
+  "Semestre 5": [
+    { nombre: "Farmacognosia", prerequisitos: ["Química Orgánica II", "Botánica"] },
+    { nombre: "Biología Molecular", prerequisitos: ["Bioquímica"] },
+    { nombre: "Operaciones Unitarias", prerequisitos: ["Fisicoquímica"] },
+    { nombre: "Asignatura Electiva Profesional", prerequisitos: ["Ética"] },
+    { nombre: "Microbiología", prerequisitos: ["Bioquímica"] },
+    { nombre: "Legislación Farmacéutica", prerequisitos: ["Administración de Organizaciones"] }
+  ],
+  "Semestre 6": [
+    { nombre: "Biofarmacia", prerequisitos: ["Análisis Instrumental", "Fisiología II"] },
+    { nombre: "Farmacología I", prerequisitos: ["Fisiopatología", "Introducción a la Química Farmacéutica"] },
+    { nombre: "Inmunología", prerequisitos: ["Biología Molecular", "Fisiopatología"] },
+    { nombre: "Química Farmacéutica II", prerequisitos: ["Introducción a la Química Farmacéutica", "Microbiología"] },
+    { nombre: "Práctica Profesional Farmacia I", prerequisitos: ["Administración de Recursos Humanos", "Ética", "Legislación Farmacéutica"] },
+    { nombre: "Bioestadística", prerequisitos: ["Matemáticas", "Ética"] }
+  ],
+  "Semestre 7": [
+    { nombre: "Tecnología Farmacéutica I", prerequisitos: ["Biofarmacia", "Operaciones Unitarias"] },
+    { nombre: "Farmacología II", prerequisitos: ["Farmacología I", "Química Farmacéutica II"] },
+    { nombre: "Química Fisiológica y Patológica", prerequisitos: ["Análisis Instrumental", "Inmunología"] },
+    { nombre: "Salud Pública y Epidemiología", prerequisitos: ["Ética", "Bioestadística"] },
+    { nombre: "Química de Alimentos", prerequisitos: ["Análisis Instrumental"] },
+    { nombre: "Metodología de la Investigación", prerequisitos: ["Ética", "Bioestadística"] },
+    { nombre: "Taller Integración Perfil de Egreso I", prerequisitos: ["Práctica Profesional Farmacia I"] }
+  ],
+  "Semestre 8": [
+    { nombre: "Tecnología Farmacéutica II", prerequisitos: ["Tecnología Farmacéutica I"] },
+    { nombre: "Farmacia Asistencial", prerequisitos: ["Salud Pública y Epidemiología"] },
+    { nombre: "Nutrición", prerequisitos: ["Química Fisiológica y Patológica", "Química de Alimentos"] },
+    { nombre: "Gestión de Calidad", prerequisitos: ["Tecnología Farmacéutica I", "Legislación Farmacéutica"] },
+    { nombre: "Bioquímica Clínica", prerequisitos: ["Química Fisiológica y Patológica"] },
+    { nombre: "Seminario de Licenciatura", prerequisitos: ["Tecnología Farmacéutica I", "Farmacología II", "Química Fisiológica y Patológica", "Salud Pública y Epidemiología", "Química de Alimentos", "Metodología de la Investigación", "Taller Integración Perfil de Egreso I"] },
+    { nombre: "Taller Integración Perfil de Egreso II", prerequisitos: ["Taller Integración Perfil de Egreso I"] }
+  ],
+  "Semestre 9": [
+    { nombre: "Farmacia Clínica I", prerequisitos: ["Tecnología Farmacéutica II", "Farmacia Asistencial", "Nutrición", "Gestión de Calidad", "Bioquímica Clínica", "Seminario de Licenciatura", "Taller Integración Perfil de Egreso II"] },
+    { nombre: "Cosmética", prerequisitos: ["Tecnología Farmacéutica II"] },
+    { nombre: "Toxicología", prerequisitos: ["Farmacología II"] },
+    { nombre: "Gestión Comercial", prerequisitos: ["Gestión de Calidad", "Administración de Recursos Humanos"] },
+    { nombre: "Medicamentos Complementarios", prerequisitos: ["Farmacología II", "Nutrición"] },
+    { nombre: "Práctica Profesional II", prerequisitos: ["Farmacología II", "Práctica Profesional Farmacia I"] },
+    { nombre: "Taller Integración Perfil de Egreso III", prerequisitos: ["Taller Integración Perfil de Egreso II"] }
+  ],
+  "Semestre 10": [
+    { nombre: "Asignatura Electiva Profesional", prerequisitos: ["Ética"] },
+    { nombre: "Farmacia Clínica", prerequisitos: ["Farmacia Clínica I"] },
+    { nombre: "Unidad de Investigación o Internado I", prerequisitos: ["Metodología de la Investigación", "Seminario de Licenciatura"] },
+    { nombre: "Práctica Profesional III", prerequisitos: ["Tecnología Farmacéutica II", "Análisis Instrumental", "Farmacia Asistencial", "Toxicología", "Química de Alimentos", "Bioquímica Clínica"] }
+  ],
+  "Semestre 11": [
+    { nombre: "Unidad de Investigación o Internado II", prerequisitos: ["Unidad de Investigación o Internado I"] }
+  ]
+};
 
 const malla = document.getElementById("malla");
 
-ramos.forEach(ramo => {
-  const div = document.createElement("div");
-  div.className = "ramo";
-  div.innerHTML = `
-    <div class="nombre">${ramo.nombre}</div>
-    <div class="prerequisitos">
-      ${ramo.prerequisitos.length > 0 ? "Requiere: " + ramo.prerequisitos.join(", ") : "Sin prerequisitos"}
-    </div>
-  `;
-  malla.appendChild(div);
+Object.entries(ramosPorSemestre).forEach(([semestre, ramos]) => {
+  const titulo = document.createElement("h2");
+  titulo.textContent = semestre;
+  malla.appendChild(titulo);
+
+  const contenedor = document.createElement("div");
+  contenedor.className = "malla";
+
+  ramos.forEach(ramo => {
+    const div = document.createElement("div");
+    div.className = "ramo";
+    div.innerHTML = `
+      <div class="nombre">${ramo.nombre}</div>
+      <div class="prerequisitos">
+        ${ramo.prerequisitos.length > 0 ? "Requiere: " + ramo.prerequisitos.join(", ") : "Sin prerequisitos"}
+      </div>
+    `;
+    div.addEventListener("click", () => {
+      div.classList.toggle("completed");
+    });
+    contenedor.appendChild(div);
+  });
+
+  malla.appendChild(contenedor);
 });
